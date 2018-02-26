67c5372b26778ce0e47022773c9827e54ddf85e1
DiamondYuan <541832074@qq.com>
2018-02-26 15:08:15 +0800

    重构代码

--- main.go
+++ main.go
@@ -1,121 +1,127 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+	"flag"
+	"log"
 	"github.com/Shopify/sarama"
-	"os"
-	"time"
 	"github.com/bsm/sarama-cluster"
+	"time"
+	"strings"
 )
 
-var asyncProducer *sarama.AsyncProducer
+const (
+	defaultKafkaAddress = "localhost:9092"
+	defaultTopic        = "test"
+	defaultGroup        = "kafka-tester"
+)
+
+var address string
 var topic string
+var group string
+
+var asyncProducer *sarama.AsyncProducer
+var consumer *cluster.Consumer
 
 func main() {
-	//os.Setenv("MODE","all")
-	//os.Setenv("KAFKA_ADDRESS","localhost:9092")
-	//os.Setenv("KAFKA_TOPIC","KAFKA_TOPIC")
-	mode := os.Getenv("MODE")
-	switch mode {
-	case "all":
-		startProducer()
-		startConsumer()
-	case "producer":
-		startProducer()
-		go sentTestMessage()
-		sentTestMessage()
-	case "consumer":
-		startConsumer()
-	default:
-		startProducer()
-		go sentTestMessage()
-		startConsumer()
+	go sentTestMessage()
+	consumeTestMessage()
+	for {
+
 	}
 }
 
-func startConsumer() {
-	groupID := "group-1"
-	address := os.Getenv("KAFKA_ADDRESS")
-	topic = os.Getenv("KAFKA_TOPIC")
-	if topic == "" || address == "" {
-		fmt.Printf("缺少kafka配置")
-		return
+func init() {
+	flag.StringVar(&address, "address", defaultKafkaAddress, "Address of kafka")
+	flag.StringVar(&topic, "topic", defaultTopic, "Topic of kafka")
+	flag.StringVar(&group, "group", defaultGroup, "Group of kafka Consumer")
+	flag.Parse()
+	log.Println("kafka address: ", address)
+	log.Println("kafka topic: ", topic)
+	log.Println("kafka group: ", group)
+	{
+		err := initConsumer()
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	}
+	{
+		err := initProducer()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+func initConsumer() error {
 	config := cluster.NewConfig()
 	config.Group.Return.Notifications = true
 	config.Consumer.Offsets.CommitInterval = 1 * time.Second
 	config.Consumer.Offsets.Initial = sarama.OffsetNewest
-	c, err := cluster.NewConsumer(strings.Split(address, ","), groupID, strings.Split(topic, ","), config)
+	c, err := cluster.NewConsumer(strings.Split(address, ","), group, strings.Split(topic, ","), config)
 	if err != nil {
-		return
+		return err
 	}
-	defer c.Close()
+	consumer = c
+	return nil
+}
+
+func initProducer() error {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	p, err := sarama.NewAsyncProducer(strings.Split(address, ","), config)
+	if err != nil {
+		return err
+	}
+	asyncProducer = &p
+	return nil
+}
+
+func consumeTestMessage() {
 	go func(c *cluster.Consumer) {
 		errors := c.Errors()
 		noti := c.Notifications()
 		for {
 			select {
 			case err := <-errors:
-				fmt.Println(err.Error())
+				log.Println(err.Error())
 			case <-noti:
 			}
 		}
-	}(c)
-	for msg := range c.Messages() {
-		fmt.Println("receive " + string(msg.Value))
-		c.MarkOffset(msg, "")
+	}(consumer)
+	for msg := range consumer.Messages() {
+		log.Println("Reveive message success " + string(msg.Value))
+		consumer.MarkOffset(msg, "")
 	}
 }
 
 func sentTestMessage() {
-	for {
-		time.Sleep(time.Second)
-		fmt.Println("send " + time.Unix(time.Now().Unix(), 0).String())
-		sendMessage(time.Unix(time.Now().Unix(), 0).String())
-	}
-}
-
-func startProducer() {
-	address := os.Getenv("KAFKA_ADDRESS")
-	topic = os.Getenv("KAFKA_TOPIC")
-	if topic == "" || address == "" {
-		fmt.Println("缺少kafka配置")
-		return
-	}
-	config := sarama.NewConfig()
-	config.Producer.Return.Successes = true
-	config.Producer.RequiredAcks = sarama.WaitForAll
-	p, err := sarama.NewAsyncProducer(strings.Split(address, ","), config)
-	if err != nil {
-		fmt.Println("kafka producer创建失败 address:%s  topic:%s", address, topic)
-		return
-	} else {
-		fmt.Println("kafka producer创建成功 address:%s  topic:%s", address, topic)
-	}
-	asyncProducer = &p
-
-}
-
-func sendMessage(message string) {
-	if asyncProducer == nil {
-		return
-	}
-	msg := &sarama.ProducerMessage{
-		Topic: topic,
-		Value: sarama.ByteEncoder(message),
-	}
-	p := *asyncProducer
 	go func(p sarama.AsyncProducer) {
 		for {
 			select {
 			case err := <-p.Errors():
 				if err != nil {
-					fmt.Println("kafka error")
+					log.Println(err)
 				}
-			case <-p.Successes():
+			case msg := <-p.Successes():
+				messageByte, _ := msg.Value.Encode()
+				log.Println("Send message success" + string(messageByte))
 			}
 		}
-	}(p)
-	p.Input() <- msg
+	}(*asyncProducer)
+	for {
+		time.Sleep(time.Second)
+		sendMessage(time.Unix(time.Now().Unix(), 0).String())
+	}
+}
+
+func sendMessage(message string) {
+	if asyncProducer == nil {
+		log.Fatal("asyncProducer is nil")
+	}
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.ByteEncoder(message),
+	}
+	(*asyncProducer).Input() <- msg
 }
